588537e1da9e09803544baa050fb4167c5c44359
Yaroslav Halchenko <debian@onerussian.com>
2011-02-23 17:42:46 -0500

    wait in the master script to confirm each run

--- master_script.sh
+++ master_script.sh
@@ -2,13 +2,22 @@
 #emacs: -*- mode: shell-script; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: t -*- 
 #ex: set sts=4 ts=4 sw=4 noet:
 set -eu
+if [ $# -ne 2 ]; then
+	echo "Provide both subject and initial run (1)"
+	exit 1
+fi
 subject=$1
-run0=1
+run0=$2
 listen=1
 
 CMD=
 for run in `seq $run0 10`; do
 	echo "======================================="
-	echo "I: Subject $subject run ${run}"
-	$CMD octave -q --eval "RET_localizer('$subject', $run, $listen)"
+	read -p "Proceed with subject $subject run ${run} ['n' to cancel]? " -n 1 proceed
+	echo
+	[ "$proceed" = 'n' ] &&	break
+	{
+		date
+		$CMD octave -q --eval "RET_localizer('$subject', $run, $listen)"
+	} 2>&1 | tee data/${subject}_$run.log
 done
