a940ddc707adfbb31581b2f6b557ccd1071722cd
Yaroslav Halchenko <debian@onerussian.com>
2011-02-23 17:08:09 -0500

    master run script sweeping through the runs

--- master_script.sh
+++ master_script.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 #emacs: -*- mode: shell-script; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: t -*- 
 #ex: set sts=4 ts=4 sw=4 noet:
+set -eu
+subject=$1
+run0=1
+listen=1
 
-run=0
 CMD=
-for s in {1..3}; do
-	run=$(($run+1))
-	echo "Run ${run} - wedges"
-	$CMD octave RET_localizerWEDGES.m
-	run=$(($run+1))
-	echo "Run ${run} - wedges"
-	$CMD octave RET_localizerWEDGES.m
-	run=$(($run+1))
-	if [ $run -gt 10 ]; then
-		echo "Exiting"
-        break
-	fi
-	echo "Run ${run} - ring"
-	$CMD octave RET_localizerRING.m
-	run=$(($run+1))
-	echo "Run ${run} - ring"
-	$CMD octave RET_localizerRING.m
+for run in `seq $run0 10`; do
+	echo "======================================="
+	echo "I: Subject $subject run ${run}"
+	$CMD octave -q --eval "RET_localizer('$subject', $run, $listen)"
 done
